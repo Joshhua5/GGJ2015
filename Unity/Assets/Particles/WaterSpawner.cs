f205327fb92fa146a2273d27479fa3a3c625b7b4
Joshua Waring <joshhua123@gmail.com>
2015-01-23 21:58:07 +0800

    Basic Particle Effects

---
+++ Unity/Assets/Particles/WaterSpawner.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterSpawner : MonoBehaviour {
+	public GameObject water;
+	// Use this for initialization
+	void Start () {
+		Instantiate (water);  
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
