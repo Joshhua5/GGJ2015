9c1cfbcac6ca47d2033e053142666bc214bb8f23
Beefstyles <glenwillis@gmail.com>
2015-01-24 21:38:45 +0800

    GameMgr and PrefabMgr Scripts
    Added GameMgr and PrefabMgr Scripts

---
+++ Unity/Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	PrefabManager prefabManager;
+	public GameObject particleSpawn;
+
+	void Start () {
+		prefabManager = GameObject.FindObjectOfType<PrefabManager> ();
+		particleSpawn = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+
+		if (Input.GetKeyDown(KeyCode.Q)){
+			particleSpawn = prefabManager.fireSpawn;
+				}
+
+		else if (Input.GetKeyDown(KeyCode.A)){
+			particleSpawn = prefabManager.smokeSpawn;
+		}
+
+		else if (Input.GetKeyDown(KeyCode.Z)){
+			particleSpawn = prefabManager.sparkSpawn;
+		}
+
+		else if (Input.GetKeyDown(KeyCode.W)){
+			particleSpawn = prefabManager.spraySpawn;
+		}
+
+		else if (Input.GetKeyDown(KeyCode.S)){
+			particleSpawn = prefabManager.waterSpawn;
+		}
+
+		/*switch (Input.GetKeyDown) {
+				case(KeyCode.Q):
+						particleSpawn = prefabManager.fireSpawn;
+						break;
+				case(KeyCode.A):
+						particleSpawn = prefabManager.smokeSpawn;
+						break;
+				case(KeyCode.Z):
+						particleSpawn = prefabManager.sparkSpawn;
+						break;
+				case(KeyCode.W):
+						particleSpawn = prefabManager.spraySpawn;
+						break;
+				case(KeyCode.S):
+						particleSpawn = prefabManager.waterSpawn;
+						break;
+						*/
+				
+				
+		if(Physics.Raycast(ray, out hit))
+		{
+			if(Input.GetMouseButtonDown(0))
+			{
+			Instantiate (particleSpawn, hit.transform.position, Quaternion.identity);
+			}
+
+	}
+}
+}
+
---
+++ Unity/Assets/Scripts/Game/PrefabManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrefabManager : MonoSingleton<PrefabManager> {
+
+	public GameObject fireSpawn;
+	public GameObject smokeSpawn;
+	public GameObject sparkSpawn;
+	public GameObject spraySpawn;
+	public GameObject waterSpawn;
+}
