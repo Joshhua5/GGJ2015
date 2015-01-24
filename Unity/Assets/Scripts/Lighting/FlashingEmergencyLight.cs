cdde14c0e496b44865e1072bd77578aae77c03fb
Beefstyles <glenwillis@gmail.com>
2015-01-24 22:21:36 +0800

    Emergency/Exit and RoomLighting added
    Inc scripts to control flashing speed

---
+++ Unity/Assets/Scripts/Lighting/FlashingEmergencyLight.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlashingEmergencyLight : MonoBehaviour {
+
+	public GameObject emergencyLightFlash;
+	private bool lightOn;
+
+	void Start () {
+	
+		emergencyLightFlash.gameObject.SetActive (true);
+		lightOn = true;
+
+	}
+	
+
+	void FixedUpdate () {
+	
+		if (lightOn) {
+			StartCoroutine(lightFlash(1));
+	}
+
+	}
+
+IEnumerator lightFlash(float flashInterval){
+
+		lightOn = false;
+		emergencyLightFlash.gameObject.SetActive (false);
+		Debug.Log ("Light Off");
+		yield return new WaitForSeconds (flashInterval);
+		emergencyLightFlash.gameObject.SetActive (true);
+		Debug.Log ("Light Off");
+		yield return new WaitForSeconds (flashInterval);
+		lightOn = true;
+	}
+
+}
---
+++ Unity/Assets/Scripts/Lighting/FlashingExitLight.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlashingExitLight : MonoBehaviour {
+
+	public GameObject emergencyLightFlash;
+	private bool lightOn;
+
+	void Start () {
+	
+		emergencyLightFlash.gameObject.SetActive (true);
+		lightOn = true;
+
+	}
+	
+
+	void FixedUpdate () {
+	
+		if (lightOn) {
+			StartCoroutine(lightFlash(0.5F));
+	}
+
+	}
+
+IEnumerator lightFlash(float flashInterval){
+
+		lightOn = false;
+		emergencyLightFlash.gameObject.SetActive (false);
+		Debug.Log ("Light Off");
+		yield return new WaitForSeconds (flashInterval);
+		emergencyLightFlash.gameObject.SetActive (true);
+		Debug.Log ("Light Off");
+		yield return new WaitForSeconds (flashInterval);
+		lightOn = true;
+	}
+
+}
