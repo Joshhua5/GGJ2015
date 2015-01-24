15b0d75146787db45df2ebc4b16176e2451b7be8
Graham Vanderplank <grummbo@gmail.com>
2015-01-24 21:04:34 +0800

    Update tiles

--- Unity/Assets/Scripts/Game/Door.cs
+++ Unity/Assets/Scripts/Game/Door.cs
@@ -35,7 +35,7 @@ public class Door : MonoBehaviour
 
         _animation.Update();
         
-        AstarPath.active.UpdateGraphs(guo);
+        //AstarPath.active.UpdateGraphs(guo);
     }
 
     // Update is called once per frame
